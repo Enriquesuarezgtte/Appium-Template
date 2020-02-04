21ab3c1bc7a6270cdd19e62e07990409dee47300
Enrique Suarez <enriquesuarezgtte@gmail.com>
2020-02-04 15:52:19 -0500

    editor config

--- src/PrincipalTest.java
+++ src/PrincipalTest.java
@@ -1,4 +1,6 @@
 import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+
 import java.net.MalformedURLException;
 import org.junit.After;
 import org.junit.Before;
@@ -6,7 +8,7 @@ import org.junit.Test;
 
 public class PrincipalTest extends Capabilities {
 
-	  private AndroidDriver driver;
+	  private AndroidDriver<AndroidElement> driver;
 
 	  @Before
 	  public void setUp() throws MalformedURLException {
