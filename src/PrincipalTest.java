f0f91c4a67cebdc1b21080aff474af14ad901fbd
Enrique Suarez <enriquesuarezgtte@gmail.com>
2020-02-06 16:14:41 -0500

    added statics imports for actions and java.json to snipped

--- src/PrincipalTest.java
+++ src/PrincipalTest.java
@@ -1,11 +1,17 @@
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
-
+import static io.appium.java_client.touch.TapOptions.tapOptions;
+import static io.appium.java_client.touch.offset.ElementOption.element;
+import static io.appium.java_client.touch.LongPressOptions.longPressOptions;
+import static java.time.Duration.ofSeconds;
 import java.net.MalformedURLException;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+/*
+snipped page: C:\Users\esuarezg\AppData\Roaming\Code\User\snippets\java.json
+*/
 public class PrincipalTest extends Capabilities {
 
 	  private AndroidDriver<AndroidElement> driver;
